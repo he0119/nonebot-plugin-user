61e55077db5430ae463dfb51c6f6958ad6bf59f0
uy/sun <hmy0119@gmail.com>
2025-05-16 15:26:59 +0800

    test: 修复在 Python 3.9 下的测试报错

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,8 +9,16 @@ from freezegun import freeze_time
 from nonebot.adapters.onebot.v11 import Adapter as OnebotV11Adapter
 from nonebot.adapters.onebot.v12 import Adapter as OnebotV12Adapter
 from nonebug import NONEBOT_INIT_KWARGS, App
+from pytest_asyncio import is_async_test
 from pytest_mock import MockerFixture
 from sqlalchemy import delete, event
+
+
+def pytest_collection_modifyitems(items: list[pytest.Item]):
+    pytest_asyncio_tests = (item for item in items if is_async_test(item))
+    session_scope_marker = pytest.mark.asyncio(loop_scope="session")
+    for async_test in pytest_asyncio_tests:
+        async_test.add_marker(session_scope_marker, append=False)
 
 
 def pytest_configure(config: pytest.Config) -> None:
